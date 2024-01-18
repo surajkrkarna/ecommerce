e25c7945fab38b81e751029e13531c885548879c
Suraj <suraj@Surajs-MacBook-Pro.local>
2024-01-18 17:21:02 +0545

    to branch tst

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,6 @@ const app = express();
 const connectors = require('../backend/config');
 const db = require('../backend/dbconnect');
 
-
+console.log('first')
 app.listen(connectors.PORT, () => console.log(`App Running in PORT ${connectors.PORT}`))
+
