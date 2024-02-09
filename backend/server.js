373a8498642c05b7c3794a343a29590a5afa7179
Suraj <suraj@Surajs-MacBook-Pro.local>
2024-02-09 20:30:19 +0545

    fronted added

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const connectors = require('../backend/config');
 require('../backend/dbconnect');
 const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-
-app.use(require('./routes'))
-app.listen(connectors.PORT, () => console.log(`App Running in PORT ${connectors.PORT}`))
+app.use(require('./routes'));
+app.listen(connectors.PORT, () => console.log(`App Running in PORT ${connectors.PORT}`));
